99bb151fc219d2fb24d01e78fc7d1f682a2d18d3
U Sen Wan <senwan96@gmail.com>
2022-01-05 09:02:17 +0600

    happy coding

---
+++ cube.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int cube(int x){
+return x*x*x;
+
+}
+int main()
+{
+int n = 1;
+while (n != 0){
+    cin >> n;
+    cout << "\tcube(" << n << ") = " << cube(n) << endl;
+}
+}
